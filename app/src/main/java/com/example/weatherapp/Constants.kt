c4684eadb3233e5d371f6f540da0e805583dcf97
Ankit Ranjan <ranjanankit1313@gmail.com>
2021-09-06 22:46:57 +0530

    Weather App

---
+++ app/src/main/java/com/example/weatherapp/Constants.kt
@@ -0,0 +1,36 @@
+package com.example.weatherapp
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+object Constants {
+
+    const val APP_ID:String="5ebab2d5baf67391dd6a3e75796555ae"
+    const val BASE_URL:String="http://api.openweathermap.org/data/"
+    const val METRIC_UNIT:String = "metric"
+
+
+    fun isNetworkAvailable(context : Context):Boolean{
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val network = connectivityManager.activeNetwork ?: return false
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when{
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                else -> false
+            }
+        }
+        else {
+            val networkInfo = connectivityManager.activeNetworkInfo
+            return networkInfo != null && networkInfo.isConnectedOrConnecting
+        }
+
+    }
+}
